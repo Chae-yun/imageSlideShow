b73e22e7582a6969d40657f7d2c0678a24077860
Chaeyun Yang <ycy6685@naver.com>
2016-05-26 20:51:33 +0900

    #3 Set Slideshow Image Distance

--- app/src/main/java/kr/hs/emirim/ycy6685/imageslideshow/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ycy6685/imageslideshow/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.ViewFlipper;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ViewFlipper viewFlip;
+    EditText editTime;
+    int time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +19,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button butStart=(Button)findViewById(R.id.but_start);//참조 변수로 선언, 버튼은 지역 변수도 괜찮아, 뷰로 반환되므로 버튼으로 형변환
         Button butStop=(Button)findViewById(R.id.but_stop);
+        viewFlip=(ViewFlipper) findViewById(R.id.view_flip);
+        editTime=(EditText)findViewById(R.id.edit_time);
         butStart.setOnClickListener(this);
         butStop.setOnClickListener(this);
-        viewFlip=(ViewFlipper) findViewById(R.id.view_flip);
-        viewFlip.setFlipInterval(1000);
+
     }
 
     public void onClick(View v) { //v는 이벤트가 발생된 객체의 참조값을 가지고 있음 이걸로 무슨 버튼인지 알아냄 id 비교해서
 
         switch(v.getId()) {
             case R.id.but_start:
+                time=(int)(Double.parseDouble(editTime.getText().toString())*1000);
+                viewFlip.setFlipInterval(time);
                 viewFlip.startFlipping();
                 break;
             case R.id.but_stop:
